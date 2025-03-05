307aad5f9d43d8207a128af2065ade8fe22de776
Martin Vysny <mavi@vaadin.com>
2025-03-05 12:09:05 +0200

    MainView: document

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -1,5 +1,6 @@
 package com.vaadin.starter.skeleton;
 
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -12,8 +13,8 @@ import com.vaadin.flow.server.Attributes;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        add(new Span("Demoes the Tab Scope"));
-        add(new Span("This route is prototype-scoped (new instance created on page reload), but the value displayed below is tab-scoped. The value should not change when the page is reloaded, but should change when the route is opened in another browser tab."));
+        add(new H3("Tab Scope"));
+        add(new Span("This route is prototype-scoped (new instance created on page reload), but the value displayed below is tab-scoped. The value should not change when the page is reloaded or navigated elsewhere, but should change when the route is opened in another browser tab."));
         final Attributes values = TabScope.getCurrent().getValues();
         Integer value = (Integer) values.getAttribute("hello");
         add(new Span("Value: " + value));
